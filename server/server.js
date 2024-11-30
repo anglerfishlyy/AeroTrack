b57ed770b6f243dac86a2d3657f9dd9cdbcecd0a
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-11-30 16:45:03 +0530

    tryingToDeploy

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'AeroTrack API is running' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
